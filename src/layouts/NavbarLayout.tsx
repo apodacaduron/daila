2180f79f7926cf0ccc36c34773406e00228c5a7a
Daniel Apodaca <daniela@zoomcatalog.com>
2022-04-02 16:31:12 -0700

    workspace switcher spacing and navbar icon buttons

--- src/layouts/NavbarLayout.tsx
+++ src/layouts/NavbarLayout.tsx
@@ -1,5 +1,7 @@
 import { ProfileMenu } from '../components/layouts'
 import { ReactComponent as DailaLogo } from '../assets/svg/daila-logo.svg'
+import { IconButton } from '@mui/material'
+import { InboxOutlined, NotificationsOutlined } from '@mui/icons-material'
 
 const NavbarLayout: React.FC = () => (
   <div className="navbar-layout">
@@ -10,6 +12,12 @@ const NavbarLayout: React.FC = () => (
     </div>
     <div className="navbar-layout__center"></div>
     <div className="navbar-layout__right">
+      <IconButton>
+        <InboxOutlined />
+      </IconButton>
+      <IconButton>
+        <NotificationsOutlined />
+      </IconButton>
       <ProfileMenu />
     </div>
   </div>
