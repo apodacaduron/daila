d0199ad0494db8c5536373b6ac16d94816bfa7bd
Daniel Apodaca <daniela@zoomcatalog.com>
2022-04-04 12:46:28 -0600

    phone number validation on create patient dialog

--- src/components/patients/CreatePatientDialog.tsx
+++ src/components/patients/CreatePatientDialog.tsx
@@ -8,11 +8,14 @@ import {
   FormHelperText,
   Grid,
   InputLabel,
+  MenuItem,
 } from '@mui/material'
 import type { DialogProps } from '@mui/material'
 import DTextField from '../../config/material-ui/DTextField'
-import { emailValidation, required } from '../../utils/validations'
+import { emailValidation, phoneValidation, required } from '../../utils/validations'
 import { useForm } from 'react-hook-form'
+import { LoadingButton } from '@mui/lab'
+import DSelect from '../../config/material-ui/DSelect'
 
 interface Props extends DialogProps {
   patient?: any
@@ -20,85 +23,102 @@ interface Props extends DialogProps {
 
 const CreatePatientDialog: React.FC<Props> = (props) => {
   const { patient, ...dialogProps } = props
-  const formInstance = useForm()
+  const formInstance = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      phoneNumber: '',
+    },
+  })
+
+  const onSubmit = formInstance.handleSubmit(async (formValues) => {
+    console.log(formValues)
+    // closeDialog()
+  })
+
+  const closeDialog = () => dialogProps.onClose?.({}, 'escapeKeyDown')
 
   return (
     <Dialog {...dialogProps}>
       <DialogTitle>Create patient</DialogTitle>
       <DialogContent dividers>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <FormControl
-              error={Boolean(formInstance.formState.errors.name)}
-              variant="standard"
-              fullWidth
-            >
-              <InputLabel shrink htmlFor="name">
-                Name
-              </InputLabel>
-              <DTextField
-                placeholder="John Doe"
-                type="name"
-                id="name"
+        <form id="form" onSubmit={onSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <FormControl
                 error={Boolean(formInstance.formState.errors.name)}
-                {...formInstance.register('name', required)}
-              />
-              <FormHelperText>
-                {formInstance.formState.errors.name?.message}
-              </FormHelperText>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl
-              error={Boolean(formInstance.formState.errors.email)}
-              variant="standard"
-              fullWidth
-            >
-              <InputLabel shrink htmlFor="email">
-                Email
-              </InputLabel>
-              <DTextField
-                placeholder="johndoe@example.com"
-                type="email"
-                id="email"
+                variant="standard"
+                fullWidth
+              >
+                <InputLabel shrink htmlFor="name">
+                  Name
+                </InputLabel>
+                <DTextField
+                  placeholder="John Doe"
+                  type="name"
+                  id="name"
+                  error={Boolean(formInstance.formState.errors.name)}
+                  {...formInstance.register('name', required)}
+                />
+                <FormHelperText>
+                  {formInstance.formState.errors.name?.message}
+                </FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl
                 error={Boolean(formInstance.formState.errors.email)}
-                {...formInstance.register('email', emailValidation)}
-              />
-              <FormHelperText>
-                {formInstance.formState.errors.email?.message}
-              </FormHelperText>
-            </FormControl>
+                variant="standard"
+                fullWidth
+              >
+                <InputLabel shrink htmlFor="email">
+                  Email
+                </InputLabel>
+                <DTextField
+                  placeholder="johndoe@example.com"
+                  type="email"
+                  id="email"
+                  error={Boolean(formInstance.formState.errors.email)}
+                  {...formInstance.register('email', emailValidation)}
+                />
+                <FormHelperText>
+                  {formInstance.formState.errors.email?.message}
+                </FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl
+                error={Boolean(formInstance.formState.errors.phoneNumber)}
+                variant="standard"
+                fullWidth
+              >
+                <InputLabel shrink htmlFor="phoneNumber">
+                  Phone number
+                </InputLabel>
+                <DTextField
+                  placeholder="+52 123-456-7890"
+                  type="tel"
+                  id="phoneNumber"
+                  error={Boolean(formInstance.formState.errors.phoneNumber)}
+                  {...formInstance.register('phoneNumber', phoneValidation)}
+                />
+                <FormHelperText>
+                  {formInstance.formState.errors.phoneNumber?.message}
+                </FormHelperText>
+              </FormControl>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <FormControl
-              error={Boolean(formInstance.formState.errors.phone)}
-              variant="standard"
-              fullWidth
-            >
-              <InputLabel shrink htmlFor="phone">
-                Phone
-              </InputLabel>
-              <DTextField
-                placeholder="614-528-59-77"
-                type="phone"
-                id="phone"
-                error={Boolean(formInstance.formState.errors.phone)}
-                {...formInstance.register('phone', required)}
-              />
-              <FormHelperText>
-                {formInstance.formState.errors.phone?.message}
-              </FormHelperText>
-            </FormControl>
-          </Grid>
-        </Grid>
+        </form>
       </DialogContent>
       <DialogActions>
-        <Button onClick={() => dialogProps.onClose?.({}, 'escapeKeyDown')}>
-          Cancel
-        </Button>
-        <Button onClick={() => dialogProps.onClose?.({}, 'escapeKeyDown')}>
+        <Button onClick={closeDialog}>Cancel</Button>
+        <LoadingButton
+          type="submit"
+          form="form"
+          loading={formInstance.formState.isSubmitting}
+        >
           Create
-        </Button>
+        </LoadingButton>
       </DialogActions>
     </Dialog>
   )
--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -3,25 +3,40 @@ import type { RegisterOptions } from 'react-hook-form'
 export enum Password {
   MinLength = 6,
 }
+export enum Phone {
+  MinLength = 10,
+}
 const emailRegExp = /\S+@\S+\.\S+/
+const phoneRegExp = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
 
 export const required: RegisterOptions = {
   required: 'This field is required',
 }
+export const phoneValidation: RegisterOptions = {
+  ...required,
+  pattern: {
+    value: phoneRegExp,
+    message: 'Please enter a valid phone number'
+  },
+  minLength: {
+    value: Phone.MinLength,
+    message: `Phone number should be at least ${Phone.MinLength} digits`,
+  },
+}
 export const passwordValidation: RegisterOptions = {
-  required: 'This field is required',
+  ...required,
   minLength: {
     value: Password.MinLength,
     message: `Password should be at least ${Password.MinLength} characters`,
   },
 }
 export const confirmPasswordValidation = (password: string): RegisterOptions => ({
-  required: 'This field is required',
+  ...required,
   validate: (value: string) =>
     value === password || 'The passwords do not match',
 })
 export const emailValidation: RegisterOptions = {
-  required: 'This field is required',
+  ...required,
   pattern: {
     value: emailRegExp,
     message: 'Please enter a valid email',
